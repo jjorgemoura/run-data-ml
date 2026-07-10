c39364307a4e340f1822b7750f631adca1e6d642
Jorge Moura <jjorge.moura@gmail.com>
2026-07-10 15:36:36 +0100

    more setup

--- scripts/fetch_race_by_athlete.py
+++ scripts/fetch_race_by_athlete.py
@@ -3,12 +3,23 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-from src.scrappers.duv.duv import fetch_event
+from src.scrappers.duv.duv import DUVService
 
 def main():
-    print(f"sdfsdfsf")
-    xpto = fetch_event()
-    print(f"event -> {xpto}")
+    event_name = "South Downs Way 100"
+    athlete_name = "Jorge Moura"
+
+    duv = DUVService()
+    sdw100_event = duv.find_event_by_name(event_name)
+    
+    asdasd = duv.fetch_all_athletes_of_event(111)
+    
+    races = duv.fetch_races_from_athlete(123)
+
+
+    for race in races:
+        # print(f"Athlete {athlete_name} ran: {race.title}, {race.year}, {race.finishTime}")
+        print(f"Athlete {athlete_name} ran: {race}")
 
 if __name__ == "__main__":
     main()
--- src/scrappers/duv/duv.py
+++ src/scrappers/duv/duv.py
@@ -1,7 +1,14 @@
 
-def fetch_event():
-    """
-        ToDo
-    """
-    
-    return "the event X"
+class DUVService:
+
+    def __init__(self) -> None:
+        pass
+
+    def fetch_races_from_athlete(self, athleteID):
+        return [1, 5, 9]
+
+    def fetch_all_athletes_of_event(self, eventID):
+        return ""
+
+    def find_event_by_name(self, eventName):
+        return ""
